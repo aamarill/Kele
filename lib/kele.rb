162324ced5a2b117514ee32325ad18872c34e065
Adan Amarillas <aamarill.engr@gmail.com>
2017-12-07 14:39:29 -0800

    Add ability to get roadmaps and checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,40 +1,43 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap'
 
 class Kele
-	include HTTParty
+  include HTTParty
+  include Roadmap
 
-	def initialize(email, password)
-		email, password = email, password
+  def initialize(email, password)
+    email, password = email, password
 
-		@base_url = "https://www.bloc.io/api/v1"
+    @base_url = "https://www.bloc.io/api/v1"
 
-		options = {
-			body: { email: email, password: password }
-		}
+    options = {
+    body: { email: email, password: password }
+    }
 
-		response = self.class.post("#{@base_url}/sessions", options)
+    response = self.class.post("#{@base_url}/sessions", options)
 
-		@auth_token = response["auth_token"]
-	end
+    @auth_token = response["auth_token"]
+  end
 
-	def get_me
-		url = "#{@base_url}/users/me"
+  def get_me
+    url = "#{@base_url}/users/me"
 
-		response = self.class.get(url, headers: { "authorization" => @auth_token })
+    response = self.class.get(url, headers: { "authorization" => @auth_token })
 
-		user_data_hash = JSON.parse(response.body)
-	end
+    user_data_hash = JSON.parse(response.body)
+  end
 
-	def get_mentor_availability
-		user_data_hash = get_me
+  def get_mentor_availability
+    user_data_hash = get_me
 
-		mentor_id = user_data_hash["current_enrollment"]["mentor_id"]
+    mentor_id = user_data_hash["current_enrollment"]["mentor_id"]
 
-		url = "#{@base_url}/mentors/#{mentor_id}/student_availability"
+    url = "#{@base_url}/mentors/#{mentor_id}/student_availability"
 
-		response = self.class.get(url, headers: { "authorization" => @auth_token })
+    response = self.class.get(url, headers: { "authorization" => @auth_token })
 
-		mentor_availability = JSON.parse(response.body)
-	end
+    mentor_availability = JSON.parse(response.body)
+  end
+
 end
