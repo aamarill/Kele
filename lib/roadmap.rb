46334636829285c1d2b4bec3953076261dbe3dbc
Adan Amarillas <aamarill.engr@gmail.com>
2017-12-08 08:41:37 -0800

    Refactor 0.0.1 - Shrink down methods.

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,21 +1,15 @@
 module Roadmap
   def get_roadmap
     user_data_hash = get_me
-
     roadmap_id = user_data_hash["current_enrollment"]["roadmap_id"]
-
     url = "#{@base_url}/roadmaps/#{roadmap_id}"
-
     response = self.class.get(url, headers: { "authorization" => @auth_token })
-
     roadmap = JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
     url = "#{@base_url}/checkpoints/#{checkpoint_id}"
-
     response = self.class.get(url, headers: { "authorization" => @auth_token })
-
     checkpoint = JSON.parse(response.body)
   end
 end
