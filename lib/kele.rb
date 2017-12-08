633be2b6402527acfca1350f23044b2613d2886b
Adan Amarillas <aamarill.engr@gmail.com>
2017-12-08 01:44:43 -0800

    Add ability to create a checkpoint submmission

--- lib/kele.rb
+++ lib/kele.rb
@@ -129,4 +129,25 @@ class Kele
 
     response = self.class.post(url,options)
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment=nil)
+    url = "#{@base_url}/checkpoint_submissions"
+    enrollment_id = get_me["current_enrollment"]["id"]
+
+    options = {
+      body: {
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "checkpoint_id": checkpoint_id,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+      },
+      headers: {
+        authorization: @auth_token
+      }
+    }
+
+    response = self.class.post(url,options)
+  end
+
 end
